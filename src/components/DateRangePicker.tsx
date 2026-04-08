8018c9238edd7eca77828ea99985afe9f3afc309
Saeideh Nezhad <sepide.eslamnezhad@gmail.com>
2026-04-08 22:12:22 +0200

    install and use react-datePicker

--- src/components/DateRangePicker.tsx
+++ src/components/DateRangePicker.tsx
@@ -1,27 +1,43 @@
 "use client";
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 interface Props {
   value: { start: string; end: string };
   onChange: (range: { start: string; end: string }) => void;
 }
 
 export default function DateRangePicker({ value, onChange }: Props) {
+  console.log("DateRangePicker value:", value);
   return (
     <div className="flex flex-col md:flex-row items-center gap-3">
-      <input
-        type="date"
-        value={value.start}
-        onChange={(e) => onChange({ ...value, start: e.target.value })}
+      <DatePicker
+        selected={value.start ? new Date(value.start) : null}
+        portalId="datepicker-portal"
+        dateFormat="yyyy-MM-dd"
         className="border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 p-1.5 lg:p-2 rounded-lg"
+        onChange={(date: Date | null) =>
+          onChange({
+            ...value,
+            start: date ? date.toISOString().split("T")[0] : "",
+          })
+        }
       />
 
       <span className="text-gray-500">→</span>
 
-      <input
-        type="date"
-        value={value.end}
-        onChange={(e) => onChange({ ...value, end: e.target.value })}
+      <DatePicker
+        selected={value.end ? new Date(value.end) : null}
+        portalId="datepicker-portal"
+        dateFormat="yyyy-MM-dd"
         className="border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 p-1.5 lg:p-2 rounded-lg"
+        onChange={(date: Date | null) =>
+          onChange({
+            ...value,
+            end: date ? date.toISOString().split("T")[0] : "",
+          })
+        }
       />
     </div>
   );
